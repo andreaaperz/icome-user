89b73ae84e5945b240eb8a23884e16ae77dff99d
Andrea Pérez <andreaa.perz@gmail.com>
2021-09-19 19:56:42 -0500

    Pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Login from "../pages/Login/Login.page";
 import Home from "../pages/Home/Home.page";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  HashRouter,
+} from "react-router-dom";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Detail from "../pages/Detail/Detail.page";
 
 function App(): JSX.Element {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/">
           <Login />
@@ -19,7 +25,7 @@ function App(): JSX.Element {
           <Detail />
         </Route>
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
